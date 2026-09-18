63459bc6bc708a493fc8bcb6fb901d7833b928e2
Jiwwi <lazyjiwwi@gmail.com>
2026-09-18 17:18:15 +0200

    move func definition to cpp file

--- uppgift1_hasardspel/Casino.h
+++ uppgift1_hasardspel/Casino.h
@@ -15,89 +15,14 @@
 class Casino
 {
 	public:
-		void GetMainMenu(Player::PlayerData& player)
-		{
-            enum MainMenu
-            {
-                MainMenu_DiceGame = 1,
-                MainMenu_OddOrEven = 2,
-                MainMenu_Blackjack = 3,
-                MainMenu_RockPaperScissors = 4,
-                MainMenu_Roulette = 5,
-                MainMenu_Statistics = 6,
-                MainMenu_LeaveCasino = 7
-            };
+		void GetMainMenu(Player::PlayerData& player);
 
-            int stats[5] =
-            {
-               Statistics::GameResult_NoStat,
-               Statistics::GameResult_NoStat,
-               Statistics::GameResult_NoStat,
-               Statistics::GameResult_NoStat,
-               Statistics::GameResult_NoStat
-            };
-
-            int activeGame = 0;
-            while (player.playing)
-            {
-                GameFunctions::ShowMainMenuText();
-                std::cin >> activeGame;
-                Player::ClearInputBuffer();
-
-                switch (activeGame)
-                {
-                    case MainMenu_DiceGame:
-                    {
-                        myDiceGame.PlayDiceGame(player, stats);
-                        break;
-                    }
-                    case MainMenu_OddOrEven:
-                    {
-                        myOddOrEven.PlayOddOrEven(player, stats);
-                        break;
-                    }
-                    case MainMenu_Blackjack:
-                    {
-                        myBlackjack.PlayBlackjack(player, stats);
-                        break;
-                    }
-                    case MainMenu_RockPaperScissors:
-                    {
-                        myRockPaperScissors.PlayRPS(player, stats);
-                        break;
-                    }
-                    case MainMenu_Roulette:
-                    {
-                        myRoulette.PlayRoulette(player, stats);
-                        break;
-                    }
-                    case MainMenu_Statistics:
-                    {
-                        Statistics::ShowStatistics(stats);
-                        break;
-                    }
-                    case MainMenu_LeaveCasino:
-                    {
-                        system("cls");
-                        std::cout << "\nYou left the casino with " << player.wallet << "kr to your name.\n\n\n";
-                        system("pause");
-                        player.playing = false;
-                        break;
-                    }
-                    default:
-                    {
-                        break;
-                    }
-                }
-            }
-		}
-
+	private:
 		Blackjack myBlackjack;
 		DiceGame myDiceGame;
 		OddEven myOddOrEven;
 		RPS myRockPaperScissors;
 		Roulette myRoulette;
 
-	private:
 };
 
