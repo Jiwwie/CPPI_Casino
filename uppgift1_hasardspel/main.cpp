f0b9e822e03d8763bf1721f815e74f967e9209d0
Jiwwi <lazyjiwwi@gmail.com>
2026-09-21 21:10:34 +0200

    validate username

--- uppgift1_hasardspel/main.cpp
+++ uppgift1_hasardspel/main.cpp
@@ -9,16 +9,45 @@ int main()
     Casino casino;
 
     bool validName = false;
-    char readName[17];
+    char readName[17] = {'\0'};
 
     do
     {
+        validName = false;
+        system("cls");
+        Player::ClearInputBuffer();
         std::cout << "Enter your name: ";
         std::cin.get(readName, 17);
-        std::cout << sizeof(readName) << '\n';
+
+        for (int i = 0; i < sizeof(readName); i++)
+        {
+            if (readName[i] == '\0' && i < 2)
+            {
+                std::cout << "name too short" << '\n';
+                break;
+            }
+            else if (readName[i] == '\0')
+            {
+                validName = true;
+            }
+
+            if (readName[i] != '\0' && i == 16)
+            {
+                std::cout << "name too long" << '\n';
+                break;
+            }
+
+            if ((readName[i] > 'z' || readName[i] < 'A') && readName[i] != '\0')
+            {
+                validName = false;
+                std::cout << "invalid character." << '\n';
+                break;
+            }
+        }
+
         system("pause");
 
-    } while (validName);
+    } while (!validName);
 
     const char* username = readName;
 
